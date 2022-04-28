ce6d4366642ca7e65d309444cf96628c781fffec
Fernando <fh1273509@gmail.com>
2022-04-28 16:29:01 -0300

    retomo codigo original

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { EmployeeService } from './employee.service';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
